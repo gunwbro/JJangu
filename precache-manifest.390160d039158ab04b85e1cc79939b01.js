17da26d6afcb8798e46ac976f4fa77567ba42906
Gunwoo Jo <gunwbro@gmail.com>
2020-01-21 22:19:47 +0900

    Updates

---
+++ precache-manifest.390160d039158ab04b85e1cc79939b01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bc0a63c3d6064eee785e49760e16dd",
+    "url": "/JJangu/index.html"
+  },
+  {
+    "revision": "5bdb33bff20a2a6b0c66",
+    "url": "/JJangu/static/js/2.95ffaedb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/JJangu/static/js/2.95ffaedb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4945140bc7e72ed7b59f",
+    "url": "/JJangu/static/js/main.3e3fca0c.chunk.js"
+  },
+  {
+    "revision": "41afb68d308c272d5b76",
+    "url": "/JJangu/static/js/runtime-main.4bcbde31.js"
+  },
+  {
+    "revision": "63c77284b76771b4ac38bcf31332d571",
+    "url": "/JJangu/static/media/0.63c77284.jpg"
+  },
+  {
+    "revision": "a5480822c39f996fb5f171fc1b04827e",
+    "url": "/JJangu/static/media/1.a5480822.jpg"
+  },
+  {
+    "revision": "416b87bfa32ad786b7c94b39e034f685",
+    "url": "/JJangu/static/media/2.416b87bf.jpg"
+  },
+  {
+    "revision": "1536fe7f396058012503254574c8052d",
+    "url": "/JJangu/static/media/wallpaper.1536fe7f.jpg"
+  }
+]);
